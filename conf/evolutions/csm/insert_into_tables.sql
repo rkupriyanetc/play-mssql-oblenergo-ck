806940ff8a4e5c8587766143e9a671329e61b223
1 <1@Handy>
2016-10-21 21:13:32 +0300

    URL to connect

---
+++ conf/evolutions/csm/insert_into_tables.sql
@@ -0,0 +1,4 @@
+/* Into Roles */
+insert into roles ( rolename ) values ( 'ADMIN' ); GO;
+insert into roles ( rolename ) values ( 'OPER' ); GO;
+insert into roles ( rolename ) values ( 'USER' ); GO;
